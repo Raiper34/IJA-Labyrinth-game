4fca8eb93f15ba842bc95e840400e0e1c93fcfc1
Filip Gulán <xgulan00@stud.fit.vutbr.cz>
2015-05-03 19:53:26 +0200

    delete script

--- ija3.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash 
-
-ulimit -v unlimited
-javac -cp .:src/* -d build $(find ./src/* | grep .java)
-cd build
-jar -cvfm ./../dest-client/ija2015-client.jar ./../src/manifest.mf $(find ./* | grep .class)
-
-java -jar ./../dest-client/ija2015-client.jar
